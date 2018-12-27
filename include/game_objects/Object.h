4acc14e438723590754c2f62d76fe6fc3fb0f8b2
TuckerDane <tuckerdwalker@gmail.com>
2018-12-26 18:25:54 -0800

    updates game loop

--- include/game_objects/Object.h
+++ include/game_objects/Object.h
@@ -17,34 +17,32 @@
 
 using std::string;
 
-class Object
-{
-protected:
-	int rowPos;											// a variable which stores the row the ant is on in the matrix
-	int colPos;											// a variable which stores the column the ant is on in the matrix
-	int weight;											// how much the object weighs
-	char symbol;										// holds the symbol of the object
-	string type;										// holds the type name of the object
-	string name;										// holds the name of the object
-	//TODO: add color attribute
+class Object {
+	protected:
+		int rowPos;											// a variable which stores the row the ant is on in the matrix
+		int colPos;											// a variable which stores the column the ant is on in the matrix
+		int weight;											// how much the object weighs
+		char symbol;										// holds the symbol of the object
+		string type;										// holds the type name of the object
+		string name;										// holds the name of the object
+		//TODO: add color attribute
 
-public:
-	Object();											// default constructor for Object
-	Object(char s, string n, int r, int c, string t);	// constructor for inherited classes
-	~Object();											// deconstructor for Object
-	//FIXME: get and set order
-	//TODO: add getColor and setColor methods
-	virtual int getRowPos();							// returns the row of the Object
-	virtual int getColPos();							// returns the column of the Object
-	virtual char getSymbol();							// returns the type of the Object
-	virtual int getWeight() = 0;						// returns the weight of the object
-	virtual void setWeight(int w) = 0;					// sets the weight of an object
-	virtual string getName() = 0;						// returns the name of the Object
-	virtual string getType();							// returns the type of Object
+	public:
+		Object();											// default constructor for Object
+		Object(char s, string n, int r, int c, string t);	// constructor for inherited classes
+		~Object();											// deconstructor for Object
+		//FIXME: get and set order
+		//TODO: add getColor and setColor methods
+		virtual int getRowPos();							// returns the row of the Object
+		virtual int getColPos();							// returns the column of the Object
+		virtual char getSymbol();							// returns the type of the Object
+		virtual int getWeight() = 0;						// returns the weight of the object
+		virtual void setWeight(int w) = 0;					// sets the weight of an object
+		virtual string getName() = 0;						// returns the name of the Object
+		virtual string getType();							// returns the type of Object
 
-	virtual void setRowPos(int r);						// set rowPos of the Object
-	virtual void setColPos(int c);						// set colPos of the Object
-
+		virtual void setRowPos(int r);						// set rowPos of the Object
+		virtual void setColPos(int c);						// set colPos of the Object
 };
 
 #endif
--- include/game_objects/Stone.h
+++ include/game_objects/Stone.h
@@ -15,21 +15,20 @@
 #include <stdlib.h>
 #include "Object.h"
 
-class Stone : public Object
-{
-private:
-	string color;
-	int weight = 10;
+class Stone : public Object {
+	private:
+		string color;
+		int weight = 10;
 
-public:
-	Stone();										// default constructor for Stone
-	Stone(int row, int col, string c);				// constructor for Stone initializing rowPos, colPos, and room
-	~Stone();										// deconstructor for Stone
-	//FIXME: get and set order
-	void setStone(int row, int col, string c);		// sets up stone
-	int getWeight();								// returns the weight of the object
-	string getName();								// returns the name of the Object
-	void setWeight(int w);							// sets the weight of an object
+	public:
+		Stone();										// default constructor for Stone
+		Stone(int row, int col, string c);				// constructor for Stone initializing rowPos, colPos, and room
+		~Stone();										// deconstructor for Stone
+		//FIXME: get and set order
+		void setStone(int row, int col, string c);		// sets up stone
+		int getWeight();								// returns the weight of the object
+		string getName();								// returns the name of the Object
+		void setWeight(int w);							// sets the weight of an object
 };
 
 #endif
