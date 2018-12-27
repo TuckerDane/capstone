bb38c81b7017a815d30533c291542891e6f14201
TuckerDane <tuckerdwalker@gmail.com>
2018-12-27 00:14:48 -0800

    fixes formatting

--- include/space_objects/Space.h
+++ include/space_objects/Space.h
@@ -24,14 +24,14 @@ using std::string;
 .............................................. */
 class Space
 {
-  protected:
+protected:
 	string spaceName; // the name of the space
 	string spaceType; // the type of space
-	int spaceNum;	 // the number associated with the space
-	int spSizeRow;	// the size of the row
-	int spSizeCol;	// the size of the column
-	Object ***space;  // a dynamically allocated 2d matrix of pointers to objects
-	Player *player;   // a pointer to a player object
+	int spaceNum;			// the number associated with the space
+	int spSizeRow;		// the size of the row
+	int spSizeCol;		// the size of the column
+	Object ***space;	// a dynamically allocated 2d matrix of pointers to objects
+	Player *player;		// a pointer to a player object
 
 	Door leftDoor; // doors
 	Door rightDoor;
@@ -43,30 +43,30 @@ class Space
 	Space *left;
 	Space *right;
 
-  public:
-	Space();						 // a constructor that initializes a 10x10 space with NULL values
+public:
+	Space();												 // a constructor that initializes a 10x10 space with NULL values
 	Space(int sizeRow, int sizeCol); // a constructor that initializes an sizeRow x siezeCol space with NULL values
-	~Space();						 // a destructor for Space
+	~Space();												 // a destructor for Space
 
-	virtual void setSpSize(int sRow, int sCol);								 // sets the size of "space[][]" to "sRow x sCol"
-	virtual void setSpPos(Object *o, int row, int col);						 // sets the value of a position in "space[][]" at "row","col" to the address of an object
+	virtual void setSpSize(int sRow, int sCol);															 // sets the size of "space[][]" to "sRow x sCol"
+	virtual void setSpPos(Object *o, int row, int col);											 // sets the value of a position in "space[][]" at "row","col" to the address of an object
 	virtual void setDoors(bool leftD, bool rightD, bool topD, bool bottomD); // a function which sets the doors for the Space
-	virtual void setSpacePtrs(Space *t, Space *b, Space *l, Space *r);		 // a function which sets the pointers (top, bottom, left and right) for the space
+	virtual void setSpacePtrs(Space *t, Space *b, Space *l, Space *r);			 // a function which sets the pointers (top, bottom, left and right) for the space
 	virtual void setSpace(string sn, Player *p, Space *l, Space *r, Space *t, Space *b, Object *o, int num) = 0;
 
 	virtual void setObjectPos(Object *o) = 0; // pure virtual function allowing the setting of a specific object
 
 	virtual Door *getDoor(string d); // gets the indicated door
 
-	virtual int getSpSizeRow();					// returns "spSizeRow"
-	virtual int getSpSizeCol();					// returns "spSizeCol"
-	virtual string getSpaceName();				// returns the name of the space
-	virtual string getSpaceType();				// returns the type of space
+	virtual int getSpSizeRow();									// returns "spSizeRow"
+	virtual int getSpSizeCol();									// returns "spSizeCol"
+	virtual string getSpaceName();							// returns the name of the space
+	virtual string getSpaceType();							// returns the type of space
 	virtual Object *getSpPos(int row, int col); // returns a pointer to what is at the position stored in the space at x, y
-	virtual void printSpace();					// prints the space
-	virtual bool movePlayer(char dir);			// a function which moves the player within the spacew
-	virtual bool enterDoorway(char dir);		// handles player moving through a door
-	virtual int getSpaceNum();					// returns the space number
+	virtual void printSpace();									// prints the space
+	virtual bool movePlayer(char dir);					// a function which moves the player within the spacew
+	virtual bool enterDoorway(char dir);				// handles player moving through a door
+	virtual int getSpaceNum();									// returns the space number
 };
 
 #endif
