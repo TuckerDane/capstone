3178a5f4a2e62df65266fc53da3b75e4b7ba845c
TuckerDane <tuckerdwalker@gmail.com>
2019-01-30 16:19:53 -0800

    adds test directory structure

---
+++ tests/run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FILE="./build/apps/adventure_tests"
+if [ -f $FILE ]; then
+   ./build/apps/adventure
+else
+   echo "File $FILE does not exist; building..."
+   make clean
+   make all
+   ./build/apps/adventure_tests
+fi
---
+++ tests/src/example_test.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("This is a test name", "[Tag]")
+{
+    
+}
