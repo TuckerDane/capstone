5df448e036e981c1683e1c26f82990900448b985
TuckerDane <tuckerdwalker@gmail.com>
2019-02-07 12:45:59 -0800

    adds trap overload constructor test and standardizes tags

--- tests/src/trap_test.cpp
+++ tests/src/trap_test.cpp
@@ -4,9 +4,23 @@
 #include "Object.hpp"
 #include "Colors.hpp"
 
-TEST_CASE("default constructor sets Trap Class variables", "[Trap][Constructor]")
+TEST_CASE("default constructor sets Trap Class variables", "[trap][constructor]")
 {
     Trap t;
+    REQUIRE( t.getName() == "Trap" );
+    REQUIRE( t.getType() == "trap_item");
+    REQUIRE( t.getWeight() == 2);
+    REQUIRE( t.getDamage() == 6);
+    REQUIRE( t.getSymbol() == 'T');
+    REQUIRE( t.getColor() == COLOR_RED);
+    REQUIRE( t.getDescription() == "Damages the player for 6 HP");
+}
+
+TEST_CASE("overload constructor sets Trap Class variables", "[trap][constructor]")
+{
+    Trap t(7, 5);
+    REQUIRE( t.getYPos() == 7);
+    REQUIRE( t.getXPos() == 5);
     REQUIRE( t.getName() == "Trap" );
     REQUIRE( t.getType() == "trap_item");
     REQUIRE( t.getWeight() == 2);
