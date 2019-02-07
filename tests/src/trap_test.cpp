22734247ec529406420ac94f9d2a04ba6504be8a
TuckerDane <tuckerdwalker@gmail.com>
2019-02-07 12:55:54 -0800

    updates trap_tests

--- tests/src/trap_test.cpp
+++ tests/src/trap_test.cpp
@@ -29,3 +29,43 @@ TEST_CASE("overload constructor sets Trap Class variables", "[trap][constructor]
     REQUIRE( t.getColor() == COLOR_RED);
     REQUIRE( t.getDescription() == "Damages the player for 6 HP");
 }
+
+TEST_CASE("set trap type", "[trap][set]")
+{
+    Trap t;
+    REQUIRE( t.getType() == "trap_item");
+    t.setType("");
+    REQUIRE( t.getType() == "");
+    t.setType("THEtrapOFTRUTH");
+    REQUIRE( t.getType() == "THEtrapOFTRUTH");
+}
+
+TEST_CASE("set trap name", "[trap][set]")
+{
+    Trap t;
+    REQUIRE ( t.getName() == "Trap");
+    t.setName("green trap");
+    REQUIRE ( t.getName() == "green trap");
+}
+
+TEST_CASE("set trap weight", "[trap][get][set]")
+{
+    Trap t;
+    REQUIRE (t.getWeight() == 2 );
+    t.setWeight(20);
+    REQUIRE (t.getWeight() == 20);
+    t.setWeight(0);
+    REQUIRE (t.getWeight() == 0);
+    t.setWeight(-49);
+    REQUIRE (t.getWeight() == -49);
+}
+
+TEST_CASE("set trap description", "[trap][get][set]")
+{
+    Trap t;
+    REQUIRE (t.getDescription() == "Damages the player for 6 HP");
+    t.setDescription("does nothing at all");
+    REQUIRE (t.getDescription() == "does nothing at all");
+    t.setDescription("");
+    REQUIRE (t.getDescription() == "");
+}
