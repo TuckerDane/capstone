3d562f3eabcf5bb1f2bb169b8e4fd86340c498db
codingmarisa <codingmarisa@gmail.com>
2019-02-06 22:18:39 -0800

    adding file

---
+++ tests/src/world_control_test.cpp
@@ -0,0 +1,61 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+// #include "Object.hpp"
+// #include "Player.hpp"
+// #include "Door.hpp"
+// #include "../../src/game_objects/Object.cpp"
+// #include "../../src/game_objects/Player.cpp"
+// #include "../../src/game_objects/Door.cpp"
+
+//SKELETON
+// TEST_CASE("TEST CASE DESCRIPTION HERE", "[KEYWORD]")
+// {
+    //Key k;
+    // REQUIRE( k.getType() == "key" );
+    // REQUIRE( k.getName() == "normal key");
+    // REQUIRE( k.getPassword() == -1);
+    // REQUIRE( k.getWeight() == 1);
+    // REQUIRE( k.getDescription() == "used to unlock doors");
+// }
+
+// Test if w || W || KEY_UP, player moves up (3 SEPERATE TESTS PER KEY)
+// TEST_CASE("w key makes player position go from (x,y) to (x,y-1)", "[movement][w]")
+// {
+//     Player p;
+//     int originalX = p.getXPos();
+//     int originalY = p.getYPos();
+
+//     p.move('w');
+//     REQUIRE( p.getXPos() == originalX );
+//     REQUIRE( p.getYPos() == (originalY - 1) );
+// }
+    
+    // unless there is a locked door
+    // unless there is a wall
+    // unless there is an immovable object
+
+
+// Test if a || A || KEY_LEFT, player moves left (3 SEPERATE TESTS PER KEY)
+
+    // unless there is a locked door
+    // unless there is a wall
+    // unless there is an immovable object
+
+
+
+// Test if s || S || KEY_DOWN, player moves down (3 SEPERATE TESTS PER KEY)
+
+    // unless there is a locked door
+    // unless there is a wall
+    // unless there is an immovable object
+
+
+
+// Test if d || D || KEY_RIGHT, player moves right (3 SEPERATE TESTS PER KEY)
+
+    // unless there is a locked door
+    // unless there is a wall
+    // unless there is an immovable object
+
+
+
