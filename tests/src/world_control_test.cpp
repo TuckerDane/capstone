738457ee977339e3b6a97cb40e976c855a07203c
codingmarisa <codingmarisa@gmail.com>
2019-02-08 08:39:05 -0800

    test if allowed move correctly reassigns player coordinates. test if move is not allowed if locked door is present

--- tests/src/world_control_test.cpp
+++ tests/src/world_control_test.cpp
@@ -1,82 +1,87 @@
 #include "catch.hpp"
 #include "Player.hpp"
-#include "../../src/game_objects/Player.cpp"
+#include "Game.hpp"
+#include "Update.hpp"
 
-//SKELETON
-// TEST_CASE("TEST CASE DESCRIPTION HERE", "[KEYWORD]")
-// {
-    //Key k;
-    // REQUIRE( k.getType() == "key" );
-    // REQUIRE( k.getName() == "normal key");
-    // REQUIRE( k.getPassword() == -1);
-    // REQUIRE( k.getWeight() == 1);
-    // REQUIRE( k.getDescription() == "used to unlock doors");
-// }
-
-// Test if w || W || KEY_UP, player moves up
-TEST_CASE("w key makes player position go from (x,y) to (x,y-1)", "[movement][UP]")
+TEST_CASE("player.move('w') key makes player position go from (x,y) to (x,y-1)", "[movement][UP]")
 {
     Player p;
     int originalX = p.getXPos();
     int originalY = p.getYPos();
-
     p.move('w');
     REQUIRE( p.getXPos() == originalX );
     REQUIRE( p.getYPos() == (originalY - 1) );
 }
-    
-    // unless there is a locked door
+
+TEST_CASE("isMoveAllowed at y-1 is false when there is a locked door", "[movement][UP]")
+{
+    Player p;
+    Game g;
+    Door(p.getYPos() - 1, p.getXPos(), -1, -1, -1, -1, true, COLOR_BLUE);
+    REQUIRE(g.isMoveAllowed(p.getYPos() - 1, p.getXPos()) == false);
+}   
     // unless there is a wall
     // unless there is an immovable object
 
-
-// Test if a || A || KEY_LEFT, player moves left (3 SEPERATE TESTS PER KEY)
-TEST_CASE("a key makes player position go from (x,y) to (x-1,y)", "[movement][LEFT]")
+TEST_CASE("player.move('a') makes player position go from (x,y) to (x-1,y)", "[movement][LEFT]")
 {
     Player p;
     int originalX = p.getXPos();
     int originalY = p.getYPos();
-
     p.move('a');
     REQUIRE( p.getXPos() == (originalX - 1) );
     REQUIRE( p.getYPos() == (originalY) );
 }
 
-    // unless there is a locked door
+TEST_CASE("isMoveAllowed at x-1 does not allow player to move left when there is a locked door", "[movement][LEFT]")
+{
+    Player p;
+    Game g;
+    Door(p.getYPos(), p.getXPos()-1, -1, -1, -1, -1, true, COLOR_BLUE);
+    REQUIRE(g.isMoveAllowed(p.getYPos(), p.getXPos()-1) == false);
+}  
+
     // unless there is a wall
     // unless there is an immovable object
 
-
-
-// Test if s || S || KEY_DOWN, player moves down (3 SEPERATE TESTS PER KEY)
-TEST_CASE("s key makes player position go from (x,y) to (x,y+1)", "[movement][DOWN]")
+TEST_CASE("player.move('s') makes player position go from (x,y) to (x,y+1)", "[movement][DOWN]")
 {
     Player p;
     int originalX = p.getXPos();
     int originalY = p.getYPos();
-
     p.move('s');
     REQUIRE( p.getXPos() == originalX );
     REQUIRE( p.getYPos() == (originalY + 1) );
 }
 
-    // unless there is a locked door
+TEST_CASE("isMoveAllowed at y+1 is false when there is a locked door", "[movement][DOWN]")
+{
+    Player p;
+    Game g;
+    Door(p.getYPos() + 1, p.getXPos(), -1, -1, -1, -1, true, COLOR_BLUE);
+    REQUIRE(g.isMoveAllowed(p.getYPos() + 1, p.getXPos()) == false);
+} 
+    
     // unless there is a wall
     // unless there is an immovable object
 
-
-
-// Test if d || D || KEY_RIGHT, player moves right (3 SEPERATE TESTS PER KEY)
-TEST_CASE("d key makes player position go from (x,y) to (x+1,y)", "[movement][RIGHT]")
+TEST_CASE("player.move('d') makes player position go from (x,y) to (x+1,y)", "[movement][RIGHT]")
 {
     Player p;
     int originalX = p.getXPos();
     int originalY = p.getYPos();
-
     p.move('d');
     REQUIRE( p.getXPos() == (originalX + 1) );
     REQUIRE( p.getYPos() == (originalY) );
 }
+
+TEST_CASE("isMoveAllowed at x+1 does not allow player to move left when there is a locked door", "[movement][RIGHT]")
+{
+    Player p;
+    Game g;
+    Door(p.getYPos(), p.getXPos()+1, -1, -1, -1, -1, true, COLOR_BLUE);
+    REQUIRE(g.isMoveAllowed(p.getYPos(), p.getXPos()+1) == false);
+}  
 
     // unless there is a locked door
     // unless there is a wall
